da66a6d21a6e5a7392c9b18e0dca1f096a12d42c
Guille814 <guilleibannez@gmail.com>
2024-10-29 21:24:58 +0100

    Contenedor trabajos responsive y pc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WorksComponent } from './works/works.component'; // Asegúrate de que la ruta sea correcta
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WorksComponent], // Importa WorksComponent aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -13,12 +14,23 @@ export class AppComponent implements OnInit {
   buttonText: string = 'Available for Work';
   showCopiedText: boolean = false;
   fadeIn: boolean = false;
+  isVisible: boolean = false; // Propiedad para controlar la visibilidad
 
   ngOnInit() {
     // Activa la clase fade-in para los elementos después de cargar el componente
     setTimeout(() => {
       this.fadeIn = true;
-    }, 100); // Puedes ajustar el tiempo si deseas un retardo adicional
+    }, 100); // Ajusta el tiempo si deseas un retardo adicional
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const scrollY = window.scrollY || window.pageYOffset;
+    if (scrollY > 50) {
+      this.isVisible = true; // Muestra la línea
+    } else {
+      this.isVisible = false; // Oculta la línea
+    }
   }
 
   showEmail() {
